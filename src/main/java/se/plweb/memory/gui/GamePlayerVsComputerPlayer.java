64b8ae2496a33873471d5bfd258b7a76c3b85fa5
plwebse <peter.lindblom@plweb.se>
2022-01-02 21:02:14 +0100

    Some refactoring

--- src/main/java/se/plweb/memory/gui/GamePlayerVsComputerPlayer.java
+++ src/main/java/se/plweb/memory/gui/GamePlayerVsComputerPlayer.java
@@ -1,99 +1,107 @@
 package se.plweb.memory.gui;
 
+import java.util.Arrays;
+import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import javax.swing.JPanel;
+
 import se.plweb.memory.domain.*;
 
 /**
- * 
  * @author Peter Lindblom
  */
 
 public class GamePlayerVsComputerPlayer extends JPanel {
 
-	private static final long serialVersionUID = 1L;
-	private static Logger logger;
-	private final GameBoardGuiPlayerVsComputerPlayer gameBoard;
-	private final GameBoard computerPlayersGameBoard = new GameBoardImpl();
-	private final PlayerStatusPanel humanPlayerStatusPanel = new PlayerStatusPanel(
-			"Human player");
-	private final PlayerStatusPanel computerPlayerStatusPanel = new PlayerStatusPanel(
-			"Computer player");
-	private final ThreadControl threadControl = ThreadControl.getInstance();
+    private static final long serialVersionUID = 1L;
+    private static Logger logger;
+    private final GameBoardGuiPlayerVsComputerPlayer gameBoard;
+    private final GameBoard computerPlayersGameBoard = new GameBoardImpl();
+    private final PlayerStatusPanel humanPlayerStatusPanel = new PlayerStatusPanel(
+            "Human player");
+    private final PlayerStatusPanel computerPlayerStatusPanel = new PlayerStatusPanel(
+            "Computer player");
+    private final ThreadControl threadControl = ThreadControl.getInstance();
 
-	public GamePlayerVsComputerPlayer(Gui gui, int xSize, int ySize,
-			ComputerPlayers difficulty) {
-		logger = Logger.getLogger(this.getClass().getName());
-		computerPlayersGameBoard.makeGameBoard(xSize, ySize);
-		computerPlayersGameBoard.startGame();
-		ComputerPlayer computerPlayer = difficulty
-				.createComputerPlayer(computerPlayersGameBoard.getTotalSize());
-		gameBoard = new GameBoardGuiPlayerVsComputerPlayer(computerPlayer,
-				computerPlayersGameBoard);
-		gameBoard.makeGameBoard(xSize, ySize);
-		gameBoard.startGame();
+    public GamePlayerVsComputerPlayer(Gui gui, int xSize, int ySize,
+                                      ComputerPlayers difficulty) {
+        logger = Logger.getLogger(this.getClass().getName());
+        computerPlayersGameBoard.makeGameBoard(xSize, ySize);
+        computerPlayersGameBoard.startGame();
+        ComputerPlayer computerPlayer = difficulty
+                .createComputerPlayer(computerPlayersGameBoard.getTotalSize());
+        gameBoard = new GameBoardGuiPlayerVsComputerPlayer(computerPlayer,
+                computerPlayersGameBoard);
+        gameBoard.makeGameBoard(xSize, ySize);
+        gameBoard.startGame();
 
-		CommonGameFunctions.applyGridBagConstraints(this, gameBoard,
-				humanPlayerStatusPanel, computerPlayerStatusPanel);
+        CommonGameFunctions.applyGridBagConstraints(this, gameBoard,
+                humanPlayerStatusPanel, computerPlayerStatusPanel);
 
-		gui.startPlayerVsComputerPlayer(this);
-	}
+        gui.startPlayerVsComputerPlayer(this);
+    }
 
-	public int getHumanPlayersMatchedPairs() {
-		return gameBoard.getNumberOfMatchedPairs();
-	}
+    public int getHumanPlayersMatchedPairs() {
+        return gameBoard.getNumberOfMatchedPairs();
+    }
 
-	public int getComputerPlayersMatchedPairs() {
-		return computerPlayersGameBoard.getNumberOfMatchedPairs();
-	}
+    public int getComputerPlayersMatchedPairs() {
+        return computerPlayersGameBoard.getNumberOfMatchedPairs();
+    }
 
-	public int getTotalNumberOfParis() {
-		return gameBoard.getTotalNumberOfPairs();
-	}
+    public int getTotalNumberOfParis() {
+        return gameBoard.getTotalNumberOfPairs();
+    }
 
-	public int getHumanPlayersNumberOfAttempts() {
-		return gameBoard.getTotalNumberOfAttempts();
-	}
+    public int getHumanPlayersNumberOfAttempts() {
+        return gameBoard.getTotalNumberOfAttempts();
+    }
 
-	public int getComputerPlayersNumberOfAttempts() {
-		logger.log(Level.FINE, "getComputerPlayersNumberOfAttempts:"
-				+ computerPlayersGameBoard.getTotalNumberOfAttempts());
-		return computerPlayersGameBoard.getTotalNumberOfAttempts();
-	}
+    public int getComputerPlayersNumberOfAttempts() {
+        logger.log(Level.FINE, "getComputerPlayersNumberOfAttempts:"
+                + computerPlayersGameBoard.getTotalNumberOfAttempts());
+        return computerPlayersGameBoard.getTotalNumberOfAttempts();
+    }
 
-	public void updateHumanPlayerStatus(int matchedPairs, int numberOfAttempts) {
-		humanPlayerStatusPanel.updatePairStatus(matchedPairs, this
-				.getTotalNumberOfParis());
-		humanPlayerStatusPanel.updateAttempts(numberOfAttempts);
-	}
+    public void updateHumanPlayerStatus(int matchedPairs, int numberOfAttempts) {
+        humanPlayerStatusPanel.updatePairStatus(matchedPairs, this
+                .getTotalNumberOfParis());
+        humanPlayerStatusPanel.updateAttempts(numberOfAttempts);
+    }
 
-	public void updateComputerPlayerStatus(int matchedPairs,
-			int numberOfAttempts) {
-		computerPlayerStatusPanel.updatePairStatus(matchedPairs, this
-				.getTotalNumberOfParis());
-		computerPlayerStatusPanel.updateAttempts(numberOfAttempts);
-	}
+    public void updateComputerPlayerStatus(int matchedPairs,
+                                           int numberOfAttempts) {
+        computerPlayerStatusPanel.updatePairStatus(matchedPairs, this
+                .getTotalNumberOfParis());
+        computerPlayerStatusPanel.updateAttempts(numberOfAttempts);
+    }
 
-	public void computerPlayerWon() {
-		String messageComputerPlayerWon = "Computer player won";
-		computerPlayerStatusPanel.updateStatus(messageComputerPlayerWon);
-		humanPlayerStatusPanel.updateStatus(messageComputerPlayerWon);
-		humanPlayerStatusPanel.updatePairStatus(0, 0);
-		computerPlayerStatusPanel.updatePairStatus(0, 0);
-		gameBoard.stopGame();
-		computerPlayersGameBoard.stopGame();
-		threadControl.stopAll();
-	}
+    public void computerPlayerWon() {
+        String messageComputerPlayerWon = "Computer player won";
+        for (PlayerStatusPanel playerStatusPanel : getPlayerStatusPanels()) {
+            playerStatusPanel.updateStatus(messageComputerPlayerWon);
+            playerStatusPanel.updatePairStatus(0, 0);
+        }
 
-	public void humanPlayerWon() {
-		String messageHumanPlayerWon = "Human player won";
-		computerPlayerStatusPanel.updateStatus(messageHumanPlayerWon);
-		humanPlayerStatusPanel.updateStatus(messageHumanPlayerWon);
-		humanPlayerStatusPanel.updatePairStatus(0, 0);
-		computerPlayerStatusPanel.updatePairStatus(0, 0);
-		gameBoard.stopGame();
-		computerPlayersGameBoard.stopGame();
-		threadControl.stopAll();
-	}
+        gameBoard.stopGame();
+        computerPlayersGameBoard.stopGame();
+        threadControl.stopAll();
+    }
+
+    public void humanPlayerWon() {
+        String messageHumanPlayerWon = "Human player won";
+        for (PlayerStatusPanel playerStatusPanel : getPlayerStatusPanels()) {
+            playerStatusPanel.updateStatus(messageHumanPlayerWon);
+            playerStatusPanel.updatePairStatus(0, 0);
+        }
+
+        gameBoard.stopGame();
+        computerPlayersGameBoard.stopGame();
+        threadControl.stopAll();
+    }
+
+    private List<PlayerStatusPanel> getPlayerStatusPanels() {
+        return Arrays.asList(computerPlayerStatusPanel, humanPlayerStatusPanel);
+    }
 }
--- src/main/java/se/plweb/memory/gui/GamePlayerVsNetworkPlayer.java
+++ src/main/java/se/plweb/memory/gui/GamePlayerVsNetworkPlayer.java
@@ -1,137 +1,149 @@
 package se.plweb.memory.gui;
 
+import java.util.Arrays;
+import java.util.List;
 import java.util.logging.Logger;
 import javax.swing.JPanel;
+
 import se.plweb.memory.domain.*;
 
 /**
- * 
  * @author Peter Lindblom
  */
 
 public class GamePlayerVsNetworkPlayer extends JPanel {
-	private static final long serialVersionUID = 1L;
-	private static Logger logger;
+    private static final long serialVersionUID = 1L;
+    private static Logger logger;
 
-	private final GameBoardGui gameBoard = new GameBoardGui();
-	private final PlayerStatusPanel playerStatusPanelServer = new PlayerStatusPanel(
-			"Server");
-	private final PlayerStatusPanel playerStatusPanelClient = new PlayerStatusPanel(
-			"Client");
-	private final Gui gui;
-	private final ThreadControl threadControl = ThreadControl.getInstance();
+    private final GameBoardGui gameBoard = new GameBoardGui();
+    private final PlayerStatusPanel playerStatusPanelServer = new PlayerStatusPanel(
+            "Server");
+    private final PlayerStatusPanel playerStatusPanelClient = new PlayerStatusPanel(
+            "Client");
+    private final Gui gui;
+    private final ThreadControl threadControl = ThreadControl.getInstance();
 
-	public GamePlayerVsNetworkPlayer(int xSize, int ySize, Gui gui, int port) {
-		logger = Logger.getLogger(this.getClass().getName());
+    public GamePlayerVsNetworkPlayer(int xSize, int ySize, Gui gui, int port) {
+        logger = Logger.getLogger(this.getClass().getName());
 
-		logger.fine("Server x=" + xSize + " y=" + ySize + " port="
-				+ port);
-		// Server
-		this.gameBoard.makeGameBoard(xSize, ySize);
-		this.gui = gui;
+        logger.fine("Server x=" + xSize + " y=" + ySize + " port="
+                + port);
+        // Server
+        this.gameBoard.makeGameBoard(xSize, ySize);
+        this.gui = gui;
 
-		CommonGameFunctions.applyGridBagConstraints(this, gameBoard,
-				playerStatusPanelServer, playerStatusPanelClient);
+        CommonGameFunctions.applyGridBagConstraints(this, gameBoard,
+                playerStatusPanelServer, playerStatusPanelClient);
 
-		this.gui.startServer(this, port, xSize, ySize);
-	}
+        this.gui.startServer(this, port, xSize, ySize);
+    }
 
-	public GamePlayerVsNetworkPlayer(Gui gui, String ip, int port) {
-		logger = Logger.getLogger(this.getClass().getName());
+    public GamePlayerVsNetworkPlayer(Gui gui, String ip, int port) {
+        logger = Logger.getLogger(this.getClass().getName());
 
-		logger.fine("Client ip=" + ip + " port=" + port);
+        logger.fine("Client ip=" + ip + " port=" + port);
 
-		// Client
-		this.gui = gui;
+        // Client
+        this.gui = gui;
 
-		CommonGameFunctions.applyGridBagConstraints(this, gameBoard,
-				playerStatusPanelServer, playerStatusPanelClient);
+        CommonGameFunctions.applyGridBagConstraints(this, gameBoard,
+                playerStatusPanelServer, playerStatusPanelClient);
 
-		this.gui.startClient(this, ip, port);
-	}
+        this.gui.startClient(this, ip, port);
+    }
 
-	public void waitingForClient() {
-		String messageWaitingForClient = "waiting for client...";
-		playerStatusPanelClient.updateStatus(messageWaitingForClient);
-		playerStatusPanelServer.updateStatus(messageWaitingForClient);
-	}
+    public void waitingForClient() {
+        String messageWaitingForClient = "waiting for client...";
+        for (PlayerStatusPanel playerStatusPanel : getPlayerStatusPanels()) {
+            playerStatusPanel.updateStatus(messageWaitingForClient);
+        }
+    }
 
-	public void notConnected() {
-		String messageNotConnected = "Not connected...";
-		playerStatusPanelClient.updateStatus(messageNotConnected);
-		playerStatusPanelServer.updateStatus(messageNotConnected);
-	}
+    public void notConnected() {
+        String messageNotConnected = "Not connected...";
+        for (PlayerStatusPanel playerStatusPanel : getPlayerStatusPanels()) {
+            playerStatusPanel.updateStatus(messageNotConnected);
+        }
+    }
 
-	public int getMatchedPairs() {
-		logger.fine("getMatchedPairs="
-				+ gameBoard.getNumberOfMatchedPairs());
-		return gameBoard.getNumberOfMatchedPairs();
-	}
+    public int getMatchedPairs() {
+        logger.fine("getMatchedPairs="
+                + gameBoard.getNumberOfMatchedPairs());
+        return gameBoard.getNumberOfMatchedPairs();
+    }
 
-	public int getTotalNumberOfAttempts() {
-		logger.fine("getMatchedPairs="
-				+ gameBoard.getTotalNumberOfAttempts());
-		return gameBoard.getTotalNumberOfAttempts();
-	}
+    public int getTotalNumberOfAttempts() {
+        logger.fine("getMatchedPairs="
+                + gameBoard.getTotalNumberOfAttempts());
+        return gameBoard.getTotalNumberOfAttempts();
+    }
 
-	public int getTotalNumberOfParis() {
-		logger.fine("getNumberOfParis"
-				+ gameBoard.getTotalNumberOfPairs());
-		return gameBoard.getTotalNumberOfPairs();
-	}
+    public int getTotalNumberOfParis() {
+        logger.fine("getNumberOfParis"
+                + gameBoard.getTotalNumberOfPairs());
+        return gameBoard.getTotalNumberOfPairs();
+    }
 
-	public void updateStatusClient(int matchedPairs, int numberOfAttempts) {
-		logger.fine("" + matchedPairs + " / "
-				+ this.getTotalNumberOfParis());
-		playerStatusPanelClient.updateStatus("");
-		playerStatusPanelClient.updatePairStatus(matchedPairs, this
-				.getTotalNumberOfParis());
-		playerStatusPanelClient.updateAttempts(numberOfAttempts);
-	}
+    public void updateStatusClient(int matchedPairs, int numberOfAttempts) {
+        logger.fine("" + matchedPairs + " / "
+                + this.getTotalNumberOfParis());
+        playerStatusPanelClient.updateStatus("");
+        playerStatusPanelClient.updatePairStatus(matchedPairs, this
+                .getTotalNumberOfParis());
+        playerStatusPanelClient.updateAttempts(numberOfAttempts);
+    }
 
-	public void updateStatusServer(int matchedPairs, int numberOfAttempts) {
-		logger.fine("" + matchedPairs + " / "
-				+ this.getTotalNumberOfParis());
-		playerStatusPanelServer.updateStatus("");
-		playerStatusPanelServer.updatePairStatus(matchedPairs, this
-				.getTotalNumberOfParis());
-		playerStatusPanelServer.updateAttempts(numberOfAttempts);
-	}
+    public void updateStatusServer(int matchedPairs, int numberOfAttempts) {
+        logger.fine("" + matchedPairs + " / "
+                + this.getTotalNumberOfParis());
+        playerStatusPanelServer.updateStatus("");
+        playerStatusPanelServer.updatePairStatus(matchedPairs, this
+                .getTotalNumberOfParis());
+        playerStatusPanelServer.updateAttempts(numberOfAttempts);
+    }
 
-	public void clientWon() {
-		String messageClientWon = "Server won";
-		playerStatusPanelClient.updateStatus(messageClientWon);
-		playerStatusPanelServer.updateStatus(messageClientWon);
-		playerStatusPanelClient.updatePairStatus(0, 0);
-		playerStatusPanelServer.updatePairStatus(0, 0);
-		gameBoard.stopGame();
-		threadControl.stopAll();
-	}
+    public void clientWon() {
+        String messageClientWon = "Server won";
+        for (PlayerStatusPanel playerStatusPanel : getPlayerStatusPanels()) {
+            playerStatusPanel.updateStatus(messageClientWon);
+            playerStatusPanel.updatePairStatus(0, 0);
+        }
 
-	public void serverWon() {
-		String messageServerWon = "Client won";
-		playerStatusPanelClient.updateStatus(messageServerWon);
-		playerStatusPanelServer.updateStatus(messageServerWon);
-		playerStatusPanelClient.updatePairStatus(0, 0);
-		playerStatusPanelServer.updatePairStatus(0, 0);
-		gameBoard.stopGame();
-		threadControl.stopAll();
-	}
+        gameBoard.stopGame();
+        threadControl.stopAll();
+    }
 
-	public void disConnected() {
-		String messageDisconnected = "Disconnected";
-		playerStatusPanelClient.updateStatus(messageDisconnected);
-		playerStatusPanelServer.updateStatus(messageDisconnected);
-		gameBoard.stopGame();
-		threadControl.stopAll();
-	}
+    public void serverWon() {
+        String messageServerWon = "Client won";
+        for (PlayerStatusPanel playerStatusPanel : getPlayerStatusPanels()) {
+            playerStatusPanel.updateStatus(messageServerWon);
+            playerStatusPanel.updatePairStatus(0, 0);
+        }
 
-	public void startGame() {
-		gameBoard.startGame();
-	}
+        gameBoard.stopGame();
+        threadControl.stopAll();
+    }
 
-	public void makeGameBoard(int xSize, int ySize) {
-		logger.fine("makeGameBoard x" + xSize + " y" + ySize);
-		gameBoard.makeGameBoard(xSize, ySize);
-	}
+    public void disConnected() {
+        String messageDisconnected = "Disconnected";
+        for (PlayerStatusPanel playerStatusPanel : getPlayerStatusPanels()) {
+            playerStatusPanel.updateStatus(messageDisconnected);
+        }
+
+        gameBoard.stopGame();
+        threadControl.stopAll();
+    }
+
+    public void startGame() {
+        gameBoard.startGame();
+    }
+
+    public void makeGameBoard(int xSize, int ySize) {
+        logger.fine("makeGameBoard x" + xSize + " y" + ySize);
+        gameBoard.makeGameBoard(xSize, ySize);
+    }
+
+    private List<PlayerStatusPanel> getPlayerStatusPanels() {
+        return Arrays.asList(playerStatusPanelClient, playerStatusPanelServer);
+    }
 }
